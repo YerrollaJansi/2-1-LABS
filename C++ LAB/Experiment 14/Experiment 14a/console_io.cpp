d2e7e2a2afc1fd389c6230293abd032d24f1e4a7
YerrollaJansi <97152015+YerrollaJansi@users.noreply.github.com>
2022-02-07 22:42:56 +0530

    console_io

---
+++ C++ LAB/Experiment 14/Experiment 14a/console_io.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+       char x;
+
+       cout<<"Enter the character:";
+       cin.get(x);
+
+       cout<<"The given Character is:";
+       cout.put(x)<<endl;
+    
+       
+       cout.write("Welcome to BVRIT Hyderabad", 10);
+
+}
